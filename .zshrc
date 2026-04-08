a958964e66f7302235d8a204345dfc2ef1454070
Mcor <193207978+Mcor52@users.noreply.github.com>
2026-04-08 18:10:50 -0400

    Did a crazy amount of theming + removed yazi to focus on nvim as primary file manager

--- .zshrc
+++ .zshrc
@@ -3,6 +3,49 @@ ZVM_VI_HIGHLIGHT_FOREGROUND=white
 ZVM_VI_HIGHLIGHT_BACKGROUND=blue
 ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
 
+# Series of colours to edit syntax highlighting before antidote loads the plugin
+typeset -A FAST_HIGHLIGHT_STYLES
+
+FAST_HIGHLIGHT_STYLES[command]='fg=#ea6962'
+FAST_HIGHLIGHT_STYLES[builtin]='fg=#ea6962'
+FAST_HIGHLIGHT_STYLES[function]='fg=#ea6962'
+
+FAST_HIGHLIGHT_STYLES[arg0]='fg=#ea6962'
+
+FAST_HIGHLIGHT_STYLES[path]='fg=#d8a567'
+FAST_HIGHLIGHT_STYLES[path_pathseparator]='fg=#d8a567'
+
+FAST_HIGHLIGHT_STYLES[string]='fg=#e78a4e'
+FAST_HIGHLIGHT_STYLES[double-quoted-argument]='fg=#e78a4e'
+FAST_HIGHLIGHT_STYLES[single-quoted-argument]='fg=#e78a4e'
+
+FAST_HIGHLIGHT_STYLES[variable]='fg=#b16286'
+FAST_HIGHLIGHT_STYLES[assign]='fg=#b16286'
+
+FAST_HIGHLIGHT_STYLES[alias]='fg=#e67e80'
+
+FAST_HIGHLIGHT_STYLES[reserved-word]='fg=#ea6962'
+FAST_HIGHLIGHT_STYLES[precommand]='fg=#ea6962'
+
+FAST_HIGHLIGHT_STYLES[comment]='fg=#7c6f64'
+
+FAST_HIGHLIGHT_STYLES[unknown-token]='fg=#ea6962'
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#7c6f64'
+
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=#1d2021,bg=#d8a567'
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=#ea6962,bold'
+
+ZVM_CURSOR_STYLE_ENABLED=true
+
+ZVM_CURSOR_STYLE_DEFAULT=block
+ZVM_CURSOR_STYLE_INSERT=beam
+ZVM_CURSOR_STYLE_VISUAL=underline
+
+ZVM_CURSOR_COLOR='#ea6962'
+
+ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT
+
 # Source (pull info from) the antidote.zsh, and load the plugin manager itself
 source /usr/share/zsh-antidote/antidote.zsh
 antidote load
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,72 @@
-return {
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optional: Configure the palette/contrast before loading
-      vim.g.gruvbox_material_background = "soft"
-      vim.g.gruvbox_material_better_performance = 1
+local function hl(group, opts)
+  vim.api.nvim_set_hl(0, group, opts)
+end
 
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
+local function apply()
+  hl("@string", { fg = "#e78a4e" })
+  hl("@string.lua", { fg = "#e78a4e" })
+  hl("@string.documentation", { fg = "#d8a567" })
+  hl("@string.escape", { fg = "#ea6962" })
+  hl("String", { fg = "#e78a4e" })
+  hl("TSString", { fg = "#e78a4e" })
+
+  hl("@variable", { fg = "#b16286" })
+  hl("@variable.lua", { fg = "#b16286" })
+  hl("@lsp.type.variable", { fg = "#b16286" })
+
+  hl("@function", { fg = "#ea6962" })
+  hl("@function.call", { fg = "#ea6962" })
+  hl("@function.call.lua", { fg = "#ea6962" })
+
+  hl("@variable.member", { fg = "#e67e80" })
+  hl("@variable.member.lua", { fg = "#e67e80" })
+  hl("@field", { fg = "#e67e80" })
+
+  hl("@property", { fg = "#e67e80" })
+  hl("@lsp.type.property", { fg = "#e67e80" })
+
+  hl("@parameter", { fg = "#d8a567" })
+  hl("@lsp.type.parameter", { fg = "#d8a567" })
+
+  hl("@keyword", { fg = "#ea6962" })
+  hl("@keyword.function", { fg = "#ea6962" })
+  hl("@keyword.return", { fg = "#ea6962" })
+
+  hl("@type", { fg = "#d8a567" })
+  hl("@type.builtin", { fg = "#d8a567" })
+
+  hl("@number", { fg = "#e78a4e" })
+  hl("@boolean", { fg = "#e78a4e" })
+
+  hl("@constant", { fg = "#e67e80" })
+  hl("@constant.builtin", { fg = "#e67e80" })
+end
+
+return {
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_background = "medium"
+    vim.g.gruvbox_material_enable_italic = 1
+
+    vim.cmd.colorscheme("gruvbox-material")
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      callback = function()
+        apply()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client then
+          client.server_capabilities.semanticTokensProvider = nil
+        end
+        vim.defer_fn(apply, 50)
+      end,
+    })
+
+    apply()
+  end,
 }
